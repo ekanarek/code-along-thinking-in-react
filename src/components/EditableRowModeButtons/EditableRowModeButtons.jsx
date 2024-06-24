647e23dea7430cb954ff5826a3dc256fc99e59f5
Emily Kanarek <emilyakanarek@gmail.com>
2024-06-24 18:19:29 -0400

    Create EditableRowModeButtons component

---
+++ src/components/EditableRowModeButtons/EditableRowModeButtons.jsx
@@ -0,0 +1,12 @@
+function EditableRowModeButtons({ isEditing }) {
+    return isEditing ? (
+        <td>
+            <button>Save</button>
+        </td>
+    ) : (
+        <td>
+            <button>Delete</button>
+            <button>Edit</button>
+        </td>
+    );
+}
