3515889b9cbe339f2b2987a2a14c44bf221da9e1
Emily Kanarek <emilyakanarek@gmail.com>
2024-06-24 18:27:07 -0400

    Create component

---
+++ src/components/EditableRateCell/EditableRateCell.jsx
@@ -0,0 +1,12 @@
+import formatCurrency from '../utils/formatCurrency.js';
+
+function EditableRateCell({ value, isEditing }) {
+    return isEditing ? (
+        <td>
+            $<input type="text" value={value} /> 
+            /hr
+        </td>
+    ) : (
+        <td>{formatCurrency(value)}/hr</td>
+    )
+}
