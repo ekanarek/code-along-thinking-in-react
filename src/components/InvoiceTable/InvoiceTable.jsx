83c4808152e2110cbf572d638387018ce6ac4c61
Emily Kanarek <emilyakanarek@gmail.com>
2024-06-25 13:24:49 -0400

    Pass through mock data

--- src/components/InvoiceTable/InvoiceTable.jsx
+++ src/components/InvoiceTable/InvoiceTable.jsx
@@ -1,28 +1,27 @@
 import './InvoiceTable.css';
-// import EditableRowModeButtons from '../EditableRowModeButtons.jsx';
-// import EditableDescriptionCell from '../EditableDescriptionCell.jsx';
-// import EditableRateCell from '../EditableRateCell.jsx';
-// import EditableHoursCell from '../EditableHoursCell.jsx';
 import InvoiceTableHeader from '../InvoiceTableHeader.jsx';
 import InvoiceTableAddButton from '../InvoiceTableAddButton.jsx';
 import InvoiceTableRow from '../InvoiceTableRow.jsx';
 
-function InvoiceTable() {
+function InvoiceTable({ initialInvoiceList }) {
+    const rows = initialInvoiceList.map((invoiceItem) => {
+        const { id, description, rate, hours } = invoiceItem;
+
+        return (
+            <InvoiceTableRow 
+                key={id} 
+                initialInvoiceData={{ description, rate, hours }}
+                initialIsEditing={false}
+            />
+        )
+    });
+    
     return (
         <table>
             <thead>
                 <InvoiceTableHeader />
             </thead>
-            <tbody>
-                <InvoiceTableRow
-                    initialInvoiceData={{ description: 'Web Development', rate: 25, hours: 10 }}
-                    initialIsEditing={false}
-                />
-                <InvoiceTableRow
-                    initialInvoiceData={{ description: 'Copywriting', rate: 20, hours: 8 }}
-                    initialIsEditing={true}
-                />
-            </tbody>
+            <tbody>{rows}</tbody>
             <tfoot>
                 <InvoiceTableAddButton />
             </tfoot>
